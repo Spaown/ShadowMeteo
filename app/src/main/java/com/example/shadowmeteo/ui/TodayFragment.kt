34298360dc39402634e95d2d78a73540246e66bf
Jean-Baptiste Dubreucq <jb@mapstr.com>
2020-09-19 12:03:42 +0200

    Set nav host fragment

--- app/src/main/java/com/example/shadowmeteo/ui/TodayFragment.kt
+++ app/src/main/java/com/example/shadowmeteo/ui/TodayFragment.kt
@@ -1,6 +1,14 @@
 package com.example.shadowmeteo.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.shadowmeteo.R
 
 class TodayFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_today, container, false)
+    }
 }
